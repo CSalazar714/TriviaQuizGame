b63051fdc08045cf992476e0b3a8108175e5f1af
CSalazar714 <95145502+CSalazar714@users.noreply.github.com>
2022-03-01 09:40:11 -0600

    Updating questionjs

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -73,19 +73,6 @@ function Question() {
     }
   }
 
-  /*
-    {
-      "category": "Entertainment: Video Games",
-      "type": "boolean",
-      "difficulty": "easy",
-      "question": "Peter Molyneux was the founder of Bullfrog Productions.",
-      "correct_answer": "True",
-      "incorrect_answers": [
-        "False"
-      ]
-    }
-  */
-
   const getClass = option => {
     if (!answerSelected) {
       return ``;
